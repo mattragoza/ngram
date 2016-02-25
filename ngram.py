2f55556873cd0077a4f800da3cec727717e414d9
mattragoza <mtr22@pitt.edu>
2016-02-25 10:08:21 -0500

    project submission commit

--- ngram.py
+++ ngram.py
@@ -2,7 +2,7 @@
 
 import sys
 import nltk
-from numpy import log2
+import numpy as np
 
 
 class UsageError(Exception):
@@ -12,7 +12,7 @@ class UsageError(Exception):
 
 class NGramModel:
 
-    def __init__(self, train_corpus, N, interp=False, smooth=False):
+    def __init__(self, N, corpus=None, interp=False, smooth=False):
 
         if N <= 0:
             raise ValueError('N must be a positive integer')
@@ -20,38 +20,110 @@ class NGramModel:
 
         # can interpolate between models from N to 1
         if interp:
-            self.tries = [NGramTrie() for i in range(N)]
-            self.lambdas = [1/N for i in range(N)] # TODO learn lambda parameters
+            self.tries = [NGramTrie(N-i) for i in range(N)]
+            self.lambdas = np.array([1/N for i in range(N)])
+            self.lambdas /= sum(self.lambdas)
 
         # or use a single model
         else:
-            self.tries = [NGramTrie()]
+            self.tries = [NGramTrie(N)]
             self.lambdas = [1]
 
-        # count ngrams in train corpus by adding to tries
+        self.smooth = smooth
+
+        if corpus:
+            self.add_corpus(corpus)
+
+    def add_corpus(self, corpus):
+
+        # count ngrams in corpus by adding to tries
         for i, trie in enumerate(self.tries):
-            for sentence in train_corpus:
+            for sentence in corpus:
                 for ngram in ngramized(sentence, self.N-i):
                     trie.add(ngram)
 
-    def train_lambda(self, dev_corpus):
-        pass #TODO
+    def optimize_lambdas(self, corpus, sample_size=100):
+        
+        if len(self.lambdas) == 1:
+            return self.lambdas
 
-    def mle(self, ngram):
+        elif len(self.lambdas) == 2:
+            param_space = []
+            for i in range(10):
+                l1 = i/10
+                l2 = 1 - l1
+                param_space.append(np.array([l1, l2]))
+
+        elif len(self.lambdas) == 3:
+            param_space = []
+            for i in range(10):
+                for j in range(10-i):
+                    l1 = i/10
+                    l2 = j/10
+                    l3 = 1 - (l1 + l2)
+                    param_space.append(np.array([l1, l2, l3]))
+
+        opt_lambdas, opt_pp = self.lambdas, float('inf')
+        for lambdas in param_space:
+            self.lambdas = lambdas
+            pp = 0
+            for i in range(sample_size):
+                sentence = np.random.choice(corpus)
+                pp += self.perplexity(sentence)
+            pp /= float(sample_size)
+            if pp < opt_pp:
+                opt_lambdas = lambdas
+                opt_pp = pp
+
+        self.lambdas = opt_lambdas
+        return self.lambdas
+
+    def pr(self, ngram):
+
         if len(ngram) != self.N:
-            raise ValueError('ngram must be of length ' + str(self.N))
-        mle = []
+            raise ValueError('expected a ' + str(self.N) + '-gram')
+        
+        pr = np.zeros(len(self.tries))
         for i, trie in enumerate(self.tries):
-            mle.append(self.lambdas[i] * trie.mle(ngram[i:]))
-        return sum(mle)
+
+            given = ngram[i:-1]
+            next_ = ngram[i:]
+
+            if self.smooth:
+
+                try:
+                    c_next = trie.get(next_).count
+                except KeyError:
+                    c_next = 0
+                try:
+                    c_given = trie.get(given).count
+                    c_next_types = len(trie.get(given).next)
+                    discount = c_next_types / (c_given + c_next_types)
+                    if c_next == 0:
+                        wb = discount / c_next_types
+                    else:
+                        wb = (1 - discount) * c_next/float(c_given)
+                except KeyError:
+                    wb = 0
+                pr[i] = wb
+            else:
+                try:
+                    c_next = trie.get(next_).count
+                    c_given = trie.get(given).count
+                    mle = c_next/float(c_given)
+                except KeyError:
+                    mle = 0
+                pr[i] = mle
+
+        return np.sum(self.lambdas * pr)
 
     def entropy(self, sentence):
 
         h_s, len_s = 0, 0
         for ngram in ngramized(sentence, self.N):
-            mle = self.mle(ngram)
-            if mle > 0:
-                h_s += -log2(mle)
+            pr = self.pr(ngram)
+            if pr > 0:
+                h_s += -np.log2(pr)
             else:
                 h_s = float('inf')
             len_s += 1
@@ -69,70 +141,73 @@ class NGramModel:
 
 class NGramTrie:
 
-    def __init__(self):
-        self.n = 0 # num instances
-        self.c = {} # next words
-
-    def __contains__(self, word):
-        return word in self.c
-
-    def __getitem__(self, word):
-        return self.c[word]
-
-    def __setitem__(self, word, value):
-        self.c[word] = value
-
-    def keys(self):
-        return list(self.c.keys())
+    def __init__(self, N):
+        if N < 0:
+            raise ValueError('N must be a non-negative integer')
+        self.N = N
+        self.count = 0
+        self.next = {}
 
     def add(self, ngram):
-        self.n += 1
-        if not ngram: 
-            return
-        word = ngram[0]
-        if word not in self:
-            self[word] = NGramTrie()
-        self[word].add(ngram[1:])
-
-    def count(self, ngram):
+        if len(ngram) != self.N:
+            raise ValueError('expected a ' + str(self.N) + '-gram')
+        self.count += 1
         if not ngram:
-            return self.n
-        word = ngram[0]
-        #if len(ngram) == 1:
-        #    return self[word].n
-        return self[word].count(ngram[1:]) 
+            return
+        if ngram[0] not in self.next:
+            self.next[ngram[0]] = NGramTrie(self.N-1)
+        self.next[ngram[0]].add(ngram[1:])
 
-    def mle(self, ngram):
-        trie = self
-        for word in ngram:
-            given_c = trie.n
-            if word in trie:
-                trie = trie[word]
-            elif '<unk>' in trie:
-                trie = trie['<unk>']
+    def get(self, ngram):
+        if not ngram:
+            return self
+        elif ngram[0] in self.next:
+            return self.next[ngram[0]].get(ngram[1:])
+        else:
+            raise KeyError('ngram not found')
+
+    def choose(self):
+        r = np.random.random() * self.count
+        for word in self.next:
+            if r <= self.next[word].count:
+                return word
             else:
-                return 0
-            curr_c = trie.n
-        return curr_c/given_c
+                r -= self.next[word].count
 
-    def __str__(self):
-        return self._str()
+    def generate(self):
+        ngram = ['<s>'] * (self.N-1)
+        sentence = ['<s>']
+        while True:
+            word = self.get(ngram).choose()
+            sentence.append(word)
+            if word == '</s>':
+                break
+            ngram.append(word)
+            ngram.pop(0)
+
+        return sentence
 
     def __repr__(self):
         return 'NGramTrie()'
 
+    def __str__(self):
+        return self._str()
+
     def _str(self, tabs=0):
         tab = '    '
-        s = 'n=' + str(self.n) + ', c={'
-        if self.c: s += '\n'
-        for word in self.c:
+        s = 'count=' + str(self.count)
+        if self.next: s += '\n'
+        for word in self.next:
             s += tab*(tabs+1) + '\'' + str(word) + '\': '
-            s += self.c[word]._str(tabs+1)
-        if self.c: s += tab*tabs
-        return s + '}\n'
+            s += self.next[word]._str(tabs+1)
+        if self.next: s += tab*tabs
+        return s + '\n'
 
 
 def ngramized(sentence, N):
+
+    if N <= 0 or N != int(N):
+        raise ValueError('N must be a positive integer')
     ngram = ['<s>'] * (N-1)
     for word in sentence[1:]:
         ngram.append(word)
@@ -140,9 +215,9 @@ def ngramized(sentence, N):
         ngram.pop(0)
 
 
-def combine_unknowns(corpus, K=1):
+def get_vocab_set(corpus, K=1):
 
-    special = ['<s>', '</s>', '<unk>']
+    special = {'<s>', '</s>', '<unk>'}
     counts = {}
     for sentence in corpus:
         for word in sentence:
@@ -151,11 +226,15 @@ def combine_unknowns(corpus, K=1):
             else:
                 counts[word] = 1
 
-    unk = {w:c for w,c in counts.items() \
-        if c <= K and w not in special}
+    return {w for w in counts \
+        if counts[w] > K or w in special}
+
+
+def replace_oov_with_unk(corpus, vocab):
+
     for sentence in corpus:
         for i, word in enumerate(sentence):
-            if word in unk:
+            if word not in vocab:
                 sentence[i] = '<unk>'
 
 
@@ -207,23 +286,31 @@ def main(argv=sys.argv):
     dev_corpus   = read_processed_text_file(dev_file)
     test_corpus  = read_processed_text_file(test_file)
 
-    combine_unknowns(train_corpus, K=1)
+    vocab = get_vocab_set(train_corpus, K=1)
+    replace_oov_with_unk(train_corpus, vocab)
+    replace_oov_with_unk(dev_corpus, vocab)
+    replace_oov_with_unk(test_corpus, vocab)
 
     if mode == '1':
-        model = NGramModel(train_corpus, N=1)
+        model = NGramModel(N=1)
+        model.add_corpus(train_corpus)
     elif mode == '2':
-        model = NGramModel(train_corpus, N=2)
+        model = NGramModel(N=2)
+        model.add_corpus(train_corpus)
     elif mode == '2s':
-        model = NGramModel(train_corpus, N=2, interp=True)
-        model.train_lambda(dev_corpus)
+        model = NGramModel(N=2, interp=True)
+        model.add_corpus(train_corpus)
+        model.optimize_lambdas(dev_corpus)
     elif mode == '3':
-        model = NGramModel(train_corpus, N=3)
+        model = NGramModel(N=3)
+        model.add_corpus(train_corpus)
     elif mode == '3s':
-        model = NGramModel(train_corpus, N=3, interp=True, smooth=True)
-        model.train_lambda(dev_corpus)
+        model = NGramModel(N=3, interp=True, smooth=True)
+        model.add_corpus(train_corpus)
+        model.optimize_lambdas(dev_corpus)
 
-    PP = [model.perplexity(s) for s in test_corpus]
-    print('\n'.join(map(str, PP)))
+    for sentence in test_corpus:
+        print(' '.join(sentence) + ' ' + str(model.perplexity(sentence)))
 
 
 if __name__ == '__main__':
